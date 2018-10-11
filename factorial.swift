272ede94e7093ae8140cc908d19c0ddb5589f20c
griffrawk <griffrawk@icloud.com>
2018-10-11 13:21:39 +0100

    Flowers

--- factorial.swift
+++ factorial.swift
@@ -1,4 +1,4 @@
-// Time for some commentary
+// Time for some commentary, of a verbose, overly flowerery nature. Flowers are part of nature, as is this code
 
 func factorial(n: Int) -> Int {
     if n <= 1 { return n }
